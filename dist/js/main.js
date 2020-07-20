3385dfaaf3a0778019a0914b13b6562bb56e5ed1
sora <shenbaoblog@gmail.com>
2020-07-20 22:09:46 +0900

    20200720_概要完成

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,13 @@
 "use strict";
 
 objectFitImages();
+jQuery(function ($) {
+  $(".js_accordion_title").on("click", function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(200);
+    /*矢印の向きを変更*/
+
+    $(this).toggleClass("open", 200);
+  });
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 objectFitImages();
+
+jQuery(function($) {
+  $(".js_accordion_title").on("click", function() {
+    /*クリックでコンテンツを開閉*/
+    $(this)
+      .next()
+      .slideToggle(200);
+    /*矢印の向きを変更*/
+    $(this).toggleClass("open", 200);
+  });
+});
