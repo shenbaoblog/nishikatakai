afa25b2167da36a8fcc30a1a1e98d7df10aceca1
sora <shenbaoblog@gmail.com>
2020-08-08 15:13:36 +0900

    add_横スクロール可能なときに横スクロール可能であることを表示

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,32 @@ import "./venders/jquery.min"; // jQuery読み込み
 import "./venders/lazyload.min"; // lazyload
 import objectFitImages from "object-fit-images"; // ポリフィル（object-fit）
 import Stickyfill from "stickyfilljs"; // ポリフィル（sticky)
+import ScrollHint from "scroll-hint"; // scroll-hint
+
+
 
 // 画像遅延読み込み
 $("img.js_lazyLoad").lazyload();
+
 // ポリフィル（object-fit）
 objectFitImages();
+
 // ポリフィル（sticky)
 const ly_navigation_sticky = document.querySelectorAll(".ly_navigation");
 Stickyfill.add(ly_navigation_sticky);
+
 // ポリフィル（sticky)
 const ly_header_sticky = document.querySelectorAll(".ly_header");
 Stickyfill.add(ly_header_sticky);
+
+// 横スクロール
+new ScrollHint(".js_scrollable", {
+  suggestiveShadow: true,
+  i18n: {
+    scrollable: "横スクロール可能",
+  },
+});
+
 
 import "./functions/toTop"; // トップへ戻るボタンを途中表示
 import "./functions/autoCloseDrawer"; // ハンバーガーメニューをクリックで自動クローズ
